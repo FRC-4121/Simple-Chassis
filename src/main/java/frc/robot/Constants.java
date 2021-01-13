4c50f8654f9465a185a7b7de70dd808e067ee2c6
JonasMuhlenkamp <jonasmuhlenkamp@gmail.com>
2021-01-12 21:32:42 -0500

    last update, 2021 VS Code for FRC is live

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -32,11 +32,11 @@ public final class Constants {
         public static final int MOTOR_COUNT = 2;
 
         //Motor Controller IDs
-        public static final int LEFT_MOTOR_1 = 2;
-        public static final int LEFT_MOTOR_2 = 3;
+        public static final int LEFT_MOTOR_1 = 1;
+        public static final int LEFT_MOTOR_2 = 0;
         public static final int LEFT_MOTOR_3 = -1;
-        public static final int RIGHT_MOTOR_1 = 6;
-        public static final int RIGHT_MOTOR_2 = 7;
+        public static final int RIGHT_MOTOR_1 = 4;
+        public static final int RIGHT_MOTOR_2 = 5;
         public static final int RIGHT_MOTOR_3 = -1;
 
         //Direction Control
